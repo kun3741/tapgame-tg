8e9fa0383a9e3990602b0f3ff61f6c6ee3f80bfd
Volodymyr <95351966+kun3741@users.noreply.github.com>
2024-06-24 13:07:28 +0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => {
         console.error('MongoDB connection error:', err);
