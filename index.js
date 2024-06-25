3fa8a2ccb30bcfe47f76c0996f65b1ff1b2f5586
Volodymyr <95351966+kun3741@users.noreply.github.com>
2024-06-26 00:34:12 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -36,38 +36,38 @@ app.get('*', (req, res) => {
 });
 
 // Маршрут для отримання користувача
-// app.get('/api/routes/user/:telegramId', async (req, res) => {
-//     const telegramId = req.params.telegramId;
-//     try {
-//         const user = await User.findOne({ telegramId });
-//         if (!user) {
-//             return res.status(404).json({ message: 'User not found' });
-//         }
-//         res.json(user);
-//     } catch (error) {
-//         console.error('Error fetching user data:', error);
-//         res.status(500).json({ message: 'Internal server error' });
-//     }
-// });
+app.get('/api/routes/user/:telegramId', async (req, res) => {
+    const telegramId = req.params.telegramId;
+    try {
+        const user = await User.findOne({ telegramId });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
 
 // Маршрут для оновлення користувача
-// app.post('/api/routes/update', async (req, res) => {
-//     const { telegramId, coins, upgradeLevel } = req.body;
-//     try {
-//         const user = await User.findOneAndUpdate(
-//             { telegramId },
-//             { coins, upgradeLevel },
-//             { new: true }
-//         );
-//         if (!user) {
-//             return res.status(404).json({ message: 'User not found' });
-//         }
-//         res.json(user);
-//     } catch (error) {
-//         console.error('Error updating user data:', error);
-//         res.status(500).json({ message: 'Internal server error' });
-//     }
-// });
+app.post('/api/routes/update', async (req, res) => {
+    const { telegramId, coins, upgradeLevel } = req.body;
+    try {
+        const user = await User.findOneAndUpdate(
+            { telegramId },
+            { coins, upgradeLevel },
+            { new: true }
+        );
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
+    } catch (error) {
+        console.error('Error updating user data:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
