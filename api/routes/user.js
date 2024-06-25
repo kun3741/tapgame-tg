01c4355b57ec5d9ef4455c899fc1c35361a39df0
Volodymyr <95351966+kun3741@users.noreply.github.com>
2024-06-26 01:01:59 +0300

    Add files via upload

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,14 +3,15 @@ const User = require('../../models/User');
 
 const router = express.Router();
 
-router.get('/user/:telegramId', async (req, res, next) => {
+router.get('/:telegramId', async (req, res, next) => {
     const telegramId = req.params.telegramId;
     console.log(`Fetching data for user with telegramId: ${telegramId}`);
     try {
         const user = await User.findOne({ telegramId });
         if (!user) {
-            console.log(`User with telegramId: ${telegramId} not found`);
-            return res.status(404).json({ message: 'User not found' });
+            console.log(`User with telegramId: ${telegramId} not found. Creating new user.`);
+            user = new User({ telegramId });
+            await user.save();
         }
         res.json(user);
     } catch (error) {
