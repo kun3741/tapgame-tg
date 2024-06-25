39cbdb52239cba5459450697c5609b2d34b9603b
Volodymyr <95351966+kun3741@users.noreply.github.com>
2024-06-25 23:32:43 +0300

    Add files via upload

---
+++ user.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const User = require('../../models/User');
+
+const router = express.Router();
+
+router.get('/:telegramId', async (req, res, next) => {
+    const telegramId = req.params.telegramId;
+    console.log(`Fetching data for user with telegramId: ${telegramId}`);
+    try {
+        const user = await User.findOne({ telegramId });
+        if (!user) {
+            console.log(`User with telegramId: ${telegramId} not found`);
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        next(error); 
+    }
+});
+
+module.exports = router;
